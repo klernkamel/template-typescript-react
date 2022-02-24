3068c1380b39fbd2d355097be79deb1898773459
klernkamel <klernkamel@mail.ru>
2022-02-24 17:02:36 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Descriptions, Row, Col, Card, Divider, Calendar, List, Avatar, Statistic, Steps } from 'antd';
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -9,53 +9,145 @@ import {
   TeamOutlined,
   UserOutlined,
   StepForwardOutlined,
+  ArrowUpOutlined
 } from '@ant-design/icons';
+
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
+const data = [
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+];
+
+const { Step } = Steps;
+
 function App() {
-  const  [collapsed,setCollapsed] = useState(false)
+  const [collapsed, setCollapsed] = useState(false)
   return (
-    <Layout style={{minHeight: '100vh'}}>
-      <Sider collapsible collapsed={collapsed} onCollapse={(collapse) => {setCollapsed(collapse)}}>
-      <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              Option 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Option 2
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9" icon={<FileOutlined />}>
-              Files
-            </Menu.Item>
-          </Menu>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={(collapse) => { setCollapsed(collapse) }}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
       </Sider>
       <Layout className='site-layout'>
-      <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              Bill is a cat. <StepForwardOutlined />
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            <>
+              <Row>
+                <Col span={24}>
+                  <Card title="Card title" bordered={false}>
+                    <Steps current={1}>
+                      <Step title="Finished" description="This is a description." />
+                      <Step title="In Progress" subTitle="Left 00:00:08" description="This is a description." />
+                      <Step title="Waiting" description="This is a description." />
+                    </Steps>
+                  </Card>
+                </Col>
+              </Row>
+              <Divider />
+              <Row gutter={16}>
+                <Col span={12}><Card title="Calendar" bordered={false}>
+                  <Calendar fullscreen={false} />
+                </Card></Col>
+                <Col span={12}><Card title="List" bordered={false}>
+                  <List
+                    itemLayout="horizontal"
+                    dataSource={data}
+                    renderItem={item => (
+                      <List.Item>
+                        <List.Item.Meta
+                          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                          title={<a href="https://ant.design">{item.title}</a>}
+                          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                        />
+                      </List.Item>
+                    )}
+                  />
+                </Card></Col>
+              </Row>
+              <Divider />
+              <Row gutter={16}>
+                <Col span={8}><Card title="Card title" bordered={false}>
+                  <Statistic
+                    title="Active"
+                    value={11.28}
+                    precision={2}
+                    valueStyle={{ color: '#3f8600' }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"
+                  />
+                </Card></Col>
+                <Col span={8}><Card title="Card title" bordered={false}>
+                  <Statistic
+                    title="Active"
+                    value={11.28}
+                    precision={2}
+                    valueStyle={{ color: '#3f8600' }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"
+                  />
+                </Card></Col>
+                <Col span={8}><Card title="Card title" bordered={false}>
+                  <Statistic
+                    title="Active"
+                    value={11.28}
+                    precision={2}
+                    valueStyle={{ color: '#3f8600' }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"
+                  />
+                </Card></Col>
+              </Row>
+              <Row>
+                <Col span={6}>col-6</Col>
+                <Col span={6}>col-6</Col>
+                <Col span={6}>col-6</Col>
+                <Col span={6}>col-6</Col>
+              </Row>
+            </>,
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
     </Layout>
   );
 }
+
 
 export default App;
